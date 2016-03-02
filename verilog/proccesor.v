fc4139ea4c9e5a168fa1b4a399399b29385e1d48
dylan-wright <dylan.wright@uky.edu>
2016-03-02 15:35:13 -0500

    added proccesor.v

---
+++ verilog/proccesor.v
@@ -0,0 +1,22 @@
+/*  
+ *  EE480 - Assignment 2: The Making Of An IDIOT
+ *  proccesor.v - top level module
+ *  Version:
+ *      03-02 : initial version
+ *
+ */
+
+`define ALUadd 3'b000
+`define ALUsub 3'b001
+`define ALUxor 3'b010
+`define ALUslt 3'b011
+`define ALUor  3'b100
+`define ALUand 3'b101
+`define ALUsll 3'b110
+`define ALUsrl 3'b111
+
+module proccesor (
+    input reset
+    );
+
+endmodule
