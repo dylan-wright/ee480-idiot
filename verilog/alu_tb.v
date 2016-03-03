bd3dc66026bb295cfcda3e8547bf7ad848968c15
dylan-wright <dylan.wright@uky.edu>
2016-03-03 12:59:25 -0500

    blank testbench

---
+++ verilog/alu_tb.v
@@ -0,0 +1,36 @@
+`include "signals.v"
+
+module alu_tb;
+    function disp;
+    input x,y,z;
+    begin
+        $display("X:%d\nY:%d\nZ:%d",x,y,z);
+    end
+    endfunction
+
+    //interface to uut
+    reg `WORD X;
+    reg `WORD Y;
+    reg `WORD Z;
+    wire `WORD z;
+    reg [2:0] ALUop;
+    alu aluuut(X,Y,ALUop,z);
+
+    integer correct, failed;
+    reg `WORD calc;
+
+    initial begin
+        correct = 0;
+        failed = 0;
+        X = 0;
+        Y = 0;
+        $dumpfile("alu_tb.vcd");
+        $dumpvars(0, alu_tb);
+
+        //wait a bit
+        #5;
+
+        $display("Testing finished with %d correct %d failed", correct, failed);
+        $finish;
+    end
+endmodule
