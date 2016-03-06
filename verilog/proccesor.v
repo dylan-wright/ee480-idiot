bf74f3301bf963e528160aadf1294630c471d54a
dylan-wright <dylan.wright@uky.edu>
2016-03-06 11:07:34 -0500

    added implementations of memory and reg file to proccesor.v

--- verilog/proccesor.v
+++ verilog/proccesor.v
@@ -12,15 +12,42 @@ module proccesor (
     input reset,
     input clk
     );
-    //ALU registers
+
+    //ALU registers and data lines
     reg `WORD X;
     reg `WORD Y;
     reg `WORD Z;
     wire [2:0] ALUop;
     wire `WORD z;
 
-    alu alumod(X,Y,ALUop,z);
+    //Register file datalines
+    wire `WORD reg_data_out;
+    reg `WORD reg_data_in;
+    reg [5:0] reg_sel;
+    reg [1:0] reg_mode;
+    reg reg_clear;
 
+    //Memory registers and data lines
+    wire `WORD mem_data_out;
+    reg `WORD mem_data_in;
+    reg [1:0] mem_mode;
+    reg `WORD mem_address;
+
+    alu alu_mod(X,
+                Y,
+                ALUop,
+                z);
+    register_file register_file_mod(reg_data_out,
+                                    reg_data_in,
+                                    reg_sel,
+                                    reg_mode,
+                                    clk,
+                                    reg_clear);
+    memory memory_mod(mem_data_out,
+                      mem_data_in,
+                      mem_mode,
+                      mem_address,
+                      clk);
     always @(posedge clk)
     begin
         Z <= z;
