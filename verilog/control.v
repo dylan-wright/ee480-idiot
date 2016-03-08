a6f7b6505525d85aab76e5c3144672c01b49aab8
dylan-wright <dylan.wright@uky.edu>
2016-03-07 21:48:14 -0500

    worked on state machine. not working yet

--- verilog/control.v
+++ verilog/control.v
@@ -2,7 +2,7 @@
 
 module control(clk, ir, bus,
                 ALUop, regMode, memMode,
-                PCBusmode, IRBusmode,
+                PCBusMode, IRBusMode,
                 MARBusMode, MDRBusMode, MDRMemMode);
     input clk;
     output `WORD bus;
@@ -10,32 +10,57 @@ module control(clk, ir, bus,
     output [1:0] regMode;
     output [1:0] memMode;
     reg [7:0] state;
+    output [1:0] PCBusMode;
+    output [1:0] IRBusMode;
+    output [1:0] MARBusMode;
+    output [1:0] MDRBusMode;
+    output [1:0] MDRMemMode;
+
+    reg [1:0] pcbm, irbm, marbm, mdrbm, mdrmm;
+
+    assign PCBusMode = pcbm;
+    assign IRBusMode = irbm;
+    assign MARBusMode = marbm;
+    assign MDRBusMode = mdrbm;
+    assign MDRMemMode = mdrmm;
 
     parameter LOADSTART_0       = 8'b00000000,
               LOADSTART_1       = 8'b00000001,
-              LOADINSTRUCT_0    = 0'b00000010,
-              LOADINSTRUCT_1    = 0'b00000011,
-              LOADINSTRUCT_2    = 0'b00000100,
-              LOADINSTRUCT_3    = 0'b00000101;
+              LOADINSTRUCT_0    = 8'b00000010,
+              LOADINSTRUCT_1    = 8'b00000011,
+              LOADINSTRUCT_2    = 8'b00000100,
+              LOADINSTRUCT_3    = 8'b00000101;
 
     always @(state) begin
         case (state)
-            LOADSTART_0:    bus = 0;
-                            PCmode = `PCBusR;
-            LOADSTART_1:    bus = 16'bZ;
-                            PCmode = 0;
-            LOADINSTRUCT_0: PCmode = `PCBusW;
-                            MARBusMode = `MARBusR;
-            LOADINSTRUCT_1: PCmode = 0;
-                            marMode = 0;
-                            memMode = `memModeOut;
-                            MDRMemMode = `MARMemR;
-            LOADINSTRUCT_2: memMode = 0;
-                            MDRMemMode = 0;
-                            MDRBusMode = `MDRBusW;
-                            IRBusMode = `IRBusR;
-            LOADINSTRUCT_3: MDRBusMode = 0;
-                            IRBusMode = 0;
+            LOADSTART_0:    begin 
+                                bus = 0;
+                                pcbm = `PCBusR;
+                            end
+            LOADSTART_1:    begin
+                                bus = 16'bZ;
+                                pscbm = 0;
+                            end
+            LOADINSTRUCT_0: begin 
+                                pcbm = `PCBusW;
+                                marbm = `MARBusR;
+                            end
+            LOADINSTRUCT_1: begin
+                                pcbm = 0;
+                                marbm = 0;
+                                memMode = `memModeOut;
+                                mdrbm = `MARBusR;
+                            end
+            LOADINSTRUCT_2: begin
+                                memMode = 0;
+                                mdrmm = 0;
+                                mdrbm = `MDRBusW;
+                                irbm = `IRBusR;
+                            end
+            LOADINSTRUCT_3: begin
+                                mdrbm = 0;
+                                irbm = 0;
+                            end
         endcase
     end
 
@@ -48,5 +73,6 @@ module control(clk, ir, bus,
             LOADINSTRUCT_2: state = LOADINSTRUCT_3;
             LOADINSTRUCT_3: state = LOADINSTRUCT_0;
             default:        state = 8'bX;
+        endcase
     end
 endmodule
